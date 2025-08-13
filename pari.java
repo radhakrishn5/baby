39da25633031dc2262d64c0dddaa19f70798877a
vaibhavi656 <vaibhaviangadi107@gmail.com>
2025-08-13 15:05:30 +0530

    Update pari.java

--- pari.java
+++ pari.java
@@ -1,3 +1,12 @@
-class A{public static void main(String[]a){}}
-class A{public static void main(String[]a){System.out.print("Hi");}}
+import java.sql.*;
+import java.util.*;
+
+class V{
+  public static void main(String[]a)throws Exception{
+    Connection c=DriverManager.getConnection("jdbc:mysql://localhost/db","u","p");
+    Scanner s=new Scanner(System.in);
+    String u=s.nextLine();
+    c.createStatement().execute("SELECT * FROM users WHERE name='"+u+"'");
+  }
+}
 
